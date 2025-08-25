6570e47dbf39411c8adf24fec5ff099eedd4bff5
Pulkit Mathur <94034822+pulkit2003@users.noreply.github.com>
2025-08-25 15:53:24 +0530

    Create Sonarcube.sh

---
+++ Sonarcube.sh
@@ -0,0 +1,5 @@
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-5.0.1.3006-linux.zip
+unzip sonar-scanner-cli-5.0.1.3006-linux.zip
+sudo mv sonar-scanner-5.0.1.3006-linux /opt/sonar-scanner
+echo 'export PATH=$PATH:/opt/sonar-scanner/bin' >> ~/.bashrc
+source ~/.bashrc
