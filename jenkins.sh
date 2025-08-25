888c4069d59f1143b7fe01615c5af15a7314b156
Pulkit Mathur <94034822+pulkit2003@users.noreply.github.com>
2025-08-25 15:51:16 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,3 +2,5 @@ curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install -y jenkins
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
