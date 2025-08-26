db75b532f80e9810dd59cf219351c29d4736bbac
Pulkit Mathur <94034822+pulkit2003@users.noreply.github.com>
2025-08-26 09:42:37 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -2,3 +2,7 @@ apt install wget -y
 apt install unzip -y
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.108/bin/apache-tomcat-9.0.108.zip
 unzip apache-tomcat-9.0.108.zip
+
+cd apache-tomcat-9.0.108/bin
+chmod +x *.sh
+./startup.sh
