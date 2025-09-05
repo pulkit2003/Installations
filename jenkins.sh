2d078bc338bba096cda7aa675ba913cd5f129df9
Pulkit Mathur <94034822+pulkit2003@users.noreply.github.com>
2025-09-05 10:58:02 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,13 @@
+# Java 17
+sudo apt-get install -y fontconfig openjdk-17-jdk
+
+# Jenkins LTS
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install -y jenkins
+
+# Maven
+sudo apt-get install -y maven
 
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
